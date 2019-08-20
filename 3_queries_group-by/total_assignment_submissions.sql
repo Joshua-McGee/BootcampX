f1ab636f251fe6222d0a0a65d0b218a424cb0f7d
Joshua-McGee <jcooler.cooler@gmail.com>
2019-08-20 01:18:28 +0000

    added new directory that has sql files that grab data from groups

---
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -0,0 +1,10 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.submission_date) as total_submissions
+  FROM cohorts
+  JOIN students ON cohorts.id = cohort_id
+  JOIN assignment_submissions ON students.id = student_id
+  GROUP BY cohorts.name 
+  ORDER BY total_submissions DESC;
+
+  -- DESC means decending aka high to low
+  
+  

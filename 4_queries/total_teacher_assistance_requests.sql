b56c19e0379c44fe6e8a52a80cd5e85079dda980
Joshua-McGee <jcooler.cooler@gmail.com>
2019-08-20 23:20:24 +0000

    added the 4_queries folder that has a number of sql selects that create different tables of data based on the name of the file

---
+++ 4_queries/total_teacher_assistance_requests.sql
@@ -0,0 +1,12 @@
+-- get the total number of assistance_requests for teachers
+-- Select the teacher's name and the total assistance requests.
+-- Since this query needs to work with any specific teacher name, use 'Waylon Boehm' for the teacher's name here.
+
+
+SELECT count(assistance_requests) as total_assistances, name
+FROM teachers
+  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+  WHERE teachers.name = 'Waylon Boehm'
+  GROUP BY teachers.name;
+  
+
